750e56a6d2e0044af0b7eb70178602be4b99cf8d
Ravi Campos <rnscampos@gmail.com>
2021-05-13 16:33:10 -0300

    about generators

---
+++ dinamic.py
@@ -0,0 +1,12 @@
+def count_week():
+    ls = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    count = 0
+    while count < 7:
+        yield ls[count]
+        count += 1
+
+week = count_week()
+for num in week:
+    print(num)
+
+print(week)
