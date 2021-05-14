4add38ca6f492a6cec4bf701c7811b870a598233
Ravi Campos <rnscampos@gmail.com>
2021-05-14 13:06:26 -0300

    dinamic

--- dinamic.py
+++ dinamic.py
@@ -1,12 +1,32 @@
-def count_week():
-    ls = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    count = 0
-    while count < 7:
-        yield ls[count]
-        count += 1
+from functools import wraps
 
-week = count_week()
-for num in week:
-    print(num)
+# def enforce_fries(fn):
+#     wraps(fn)
+#     def wrapper(*args, **kwargs):
+#         if args[0] != "fries":
+#             return "You must love fries before everything else"
+#         return fn(*args, **kwargs) 
+#     return wrapper
 
-print(range())
+
+# @enforce_fries
+# def eat(first_dish, second_dish):
+#     return f"Hello, I may not know you well, but let me say that I love {first_dish} and {second_dish}"
+
+# print(eat("frie", "bacon"))
+
+def say_two(fn):
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs) + " and hello to you too Tim!"
+    return wrapper
+
+@say_two
+def say_one(name, place):
+    return f"Hello {name} from {place}"
+
+# greet = say_two(say_one)
+# print(greet("Mary"))
+
+# print(say_two(say_one)("Mary"))
+
+print(say_one("Tammy", "York"))
