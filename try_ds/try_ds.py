808a14f4138c606ccbe96c0b79988633e719882f
Ravi Campos <rnscampos@gmail.com>
2021-06-10 13:13:51 -0300

    try

--- try_ds/try_ds.py
+++ try_ds/try_ds.py
@@ -4,7 +4,13 @@ import pandas as pd
 data = pd.read_csv("./kc_house_data.csv")
 
 # data["date"] = pd.to_datetime( data["date"])
-data["bedrooms"] = data["bedrooms"].astype( float)
-data["bedrooms"] = data["bedrooms"].astype( int64 )
+# data["bedrooms"] = data["bedrooms"].astype( float)
+# data["bedrooms"] = data["bedrooms"].astype( int64 )
+# print(data.dtypes)
 
-print(data.dtypes)
+data["level"] = "standard"
+
+data.loc[data["price"] > 540000, "level"] = "high_standard"
+data.loc[data["price"] <= 540000, "level"] = "low_standard"
+
+print(data.head(10))
