4615b1bb93a615b77914028e946fd55d5d563338
Ravi Campos <rnscampos@gmail.com>
2021-06-17 18:50:33 -0300

    begining

--- streamlit_test.py
+++ streamlit_test.py
@@ -19,6 +19,8 @@ import time
 # '...and now we\'re done!'
 
 
+
+
 # use a decorator to mantain the data in ram, not in hd
 @st.cache(allow_output_mutation=True)
 def load_data(path):
@@ -28,6 +30,40 @@ data = load_data("./try_ds/kc_house_data.csv")
 
 st.title("A new chapter begins")
 
+# add new feature
+data["price/sqft"] = data["price"] / data["sqft_lot"]
+
+
+# =========================
+# Data Overview
+# =========================
+
+# create sidebar filter options (just creating the element for now)
+f_attributes = st.sidebar.multiselect("Pick an attribute", data.columns)
+f_zip_code = st.sidebar.multiselect("Pick a zipcode", data["zipcode"].unique())
+
+st.write("## Data Overview")
+
+filtered_data = None
+
+if (f_attributes != []) & (f_zip_code != []):
+    filtered_data = data.loc[data["zipcode"].isin(f_zip_code), f_attributes]
+elif (f_attributes != []) & (f_zip_code == []):
+    filtered_data = data[f_attributes]
+elif (f_attributes == []) & (f_zip_code != []):
+    filtered_data = data.loc[data["zipcode"].isin(f_zip_code), :]
+else:
+    filtered_data = data.copy()
+
+# average metrics
+df1 = filtered_data[["id", "zipcode"]].groupby("zipcode").count().reset_index()
+df2 = filtered_data[["price", "zipcode"]].groupby("zipcode").mean().reset_index()
+df3 = filtered_data[["sqft_living", "zipcode"]].groupby("zipcode").mean().reset_index()
+df4 = data[["price/sqft", "zipcode"]].groupby("zipcode").mean().reset_index()
+
+
+
+st.write(filtered_data.head())
 
 
 
