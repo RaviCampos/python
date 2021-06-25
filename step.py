bdd91a53616efa41fe155304dc4f956536d45d53
Ravi Campos <rnscampos@gmail.com>
2021-06-25 18:37:09 -0300

    trying out

--- step.py
+++ step.py
@@ -1,20 +1,20 @@
-try:
-    inp = int(input("what day is today?"));
-except:
-    inp = "not a number"
+# try:
+#     inp = int(input("what day is today?"));
+# except:
+#     inp = "not a number"
 
-if(type(inp) == str):
-    print(inp)
-else:
-    if(inp == 31):
-        out = 1
-    else:   
-        out = inp + 1
+# if(type(inp) == str):
+#     print(inp)
+# else:
+#     if(inp == 31):
+#         out = 1
+#     else:   
+#         out = inp + 1
 
-    print("tomorrow will be", out)
+#     print("tomorrow will be", out)
 
-marcia = None
-print(marcia)
+# marcia = None
+# print(marcia)
 
 # dic = {}
 
@@ -22,10 +22,18 @@ print(marcia)
 
 # print(dic)
 
-def divide_by_two(num):
-    return num / 2;
+# def divide_by_two(num):
+#     return num / 2;
 
-print(divide_by_two(4))
+# print(divide_by_two(4))
 
 
-open
+# open
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    remainder = a % b
+    return gcd(b, remainder)
+
+print(gcd(3918848, 1653264))
